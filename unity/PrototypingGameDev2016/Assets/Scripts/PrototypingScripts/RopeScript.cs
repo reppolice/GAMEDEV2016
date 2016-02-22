6152f913445d7d614b75b9af5b78f47650865676
Patrikk Dyrberg Sørensen <pdyr@itu.dk>
2016-02-22 15:50:08 +0100

    Created a prototype with a functional rope

---
+++ unity/PrototypingGameDev2016/Assets/Scripts/PrototypingScripts/RopeScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeScript : MonoBehaviour {
+
+    public Transform startPos;
+    public Transform endPos;
+
+    Transform firstRopeJoint;   
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(0, 10, 0);
+    }
+}
