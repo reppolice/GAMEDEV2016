bd87cace2802bfdee72cd0ba20d0ef276d3c2d48
Patrikk Dyrberg Sørensen <pdyr@itu.dk>
2016-02-25 10:11:00 +0100

    added new prototypes, and implemented stick-to-wall and enhance string mechanics

---
+++ unity/PrototypingGameDev2016/Assets/Scripts/PrototypingScripts/EnhanceSpringScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnhanceSpringScript : MonoBehaviour {
+    public GameObject bigBall;
+    public GameObject smallBall;
+    public float springEffect;
+    public float defaultSpring; 
+
+    SpringJoint springJoint;
+
+    void Start()
+    {
+        springJoint = bigBall.GetComponent<SpringJoint>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            springJoint.spring = springEffect;
+        } else
+        {
+            springJoint.spring = defaultSpring;
+        }
+    }
+
+}
--- unity/PrototypingGameDev2016/Assets/Scripts/PrototypingScripts/StickToWall.cs
+++ unity/PrototypingGameDev2016/Assets/Scripts/PrototypingScripts/StickToWall.cs
@@ -3,4 +3,31 @@ using System.Collections;
 
 public class StickToWall : MonoBehaviour {
 
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (gameObject.GetComponent<FixedJoint>() && !Input.GetKey(KeyCode.V))
+        {
+            Destroy(gameObject.GetComponent<FixedJoint>());
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (Input.GetKey(KeyCode.V))
+        {
+            FixedJoint joint = gameObject.AddComponent<FixedJoint>();
+            joint.connectedBody = collision.rigidbody;
+        } else if(gameObject.GetComponent<FixedJoint>())
+        {
+            Destroy(gameObject.GetComponent<FixedJoint>());
+        }
+    }
 }
