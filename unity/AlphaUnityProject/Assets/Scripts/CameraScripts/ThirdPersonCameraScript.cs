4d6d2fbda928fba944f1c2715aba7073f06d5653
Wintermotes <patri-kk@hotmail.com>
2016-03-02 19:53:48 +0100

    Initial camera and character controller

--- unity/AlphaUnityProject/Assets/Scripts/CameraScripts/ThirdPersonCameraScript.cs
+++ unity/AlphaUnityProject/Assets/Scripts/CameraScripts/ThirdPersonCameraScript.cs
@@ -10,24 +10,39 @@ public class ThirdPersonCameraScript : MonoBehaviour {
     [SerializeField]
     private float smooth;
 
-    private Transform player;
+    [SerializeField]
+    private float maxPlayerDistance; 
+
+    private Transform playerOne;
+    private Transform playerTwo;
+    private Vector3 middlePosition; 
     private Vector3 targetPosition; 
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerOne = GameObject.FindGameObjectWithTag("PlayerOne").transform;
+        playerTwo = GameObject.FindGameObjectWithTag("PlayerTwo").transform;
     }
 
     void FixedUpdate()
     {
-        targetPosition = player.position + Vector3.up * distanceUp - player.forward * distanceAway;
-        Debug.DrawRay(player.position, Vector3.up * distanceUp, Color.red);
-        Debug.DrawRay(player.position, -1f * player.forward * distanceAway, Color.blue );
-        Debug.DrawLine(player.position, targetPosition, Color.green);
+        middlePosition = findMiddlePosition(playerOne.position, playerTwo.position);
+        targetPosition = middlePosition + Vector3.up * distanceUp - playerOne.forward - playerTwo.forward * distanceAway;
+        
+
+        Debug.DrawRay(playerOne.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(playerOne.position, -1f * playerOne.forward * distanceAway, Color.blue );
+        Debug.DrawLine(playerOne.position, targetPosition, Color.green);
+        Debug.DrawLine(playerOne.position, middlePosition);
 
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+        transform.LookAt(middlePosition);
+    }
 
-        transform.LookAt(player);
+    Vector3 findMiddlePosition(Vector3 vec1, Vector3 vec2)
+    {
+        Vector3 middlePosition = (vec1 + vec2) / 2.0f;
+        return middlePosition;
     }
 
 }
