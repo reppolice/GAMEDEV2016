d9b4766365c7735e61fafa928ea2a6e680f74b12
Patrikk Dyrberg Sørensen <pdyr@itu.dk>
2016-03-22 12:34:38 +0100

    Added DOTween once again...

--- unity/AlphaUnityProject/Assets/Scripts/CameraScripts/CameraFollow.cs
+++ unity/AlphaUnityProject/Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
     public Transform player;
-    private Vector3 offset;
     public float smooth = 1.5f;         // The relative speed at which the camera will catch up
+
+    private Vector3 offset;
+    //TODO: Integrate smooth
 
     void Start()
     {
@@ -17,12 +19,14 @@ public class CameraFollow : MonoBehaviour {
     void FixedUpdate()
     {
         transform.position = player.position + offset;
+        Debug.DrawLine(transform.position, player.position); 
     }
 
-
-
-
-
-
+    public void changeOffset(float x, float y, float z)
+    {
+        offset.x += x;
+        offset.y += y;
+        offset.z += z;
+    }
 }
 
