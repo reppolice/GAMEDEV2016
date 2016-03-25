dbc97073afb8a6923f476f1e4962c7e47b9ff51a
Wintermotes <patri-kk@hotmail.com>
2016-03-25 17:40:58 +0100

    Added navmeshing agents that sticks to the player, and a scene for showcasing the event

---
+++ unity/AlphaUnityProject/Assets/Scripts/AIScripts/EnemyNavmeshScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyNavmeshScript : MonoBehaviour {
+
+    //TODO: Find optimal speed
+    public float speed = 10.0f; 
+
+    private NavMeshAgent agent;
+    private GameObject destination; 
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+    }
+
+    public void MoveToTarget(Vector3 destination)
+    {
+        agent.destination = destination; 
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with: " + collision.gameObject.name);
+        if(collision.gameObject.tag == "B4")
+        {
+            AttachStickySphere(collision.gameObject); 
+        }
+    }
+
+    void AttachStickySphere(GameObject target)
+    {
+        // Adjust sphere behavioural components
+        GetComponent<NavMeshAgent>().enabled = false;
+
+        // Child gameObject and collision
+        transform.position = target.transform.position + new Vector3(0.0f, 0.5f, 0.0f) + new Vector3(0.0f, 0.0f, 0.2f);
+        transform.parent = target.transform;
+
+        // Change material 
+        GetComponent<Renderer>().material.color = Color.black;
+
+        // SFX 
+
+        // Edit player speed, and set status 
+        target.GetComponent<PlayerController>().speed += -2.0f; 
+
+        // Adjust ambient light  
+    }
+
+    public void DetachStickySphere()
+    {
+
+    }
+}
---
+++ unity/AlphaUnityProject/Assets/Scripts/EventScripts/EnemyEventScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyEventScript : MonoBehaviour {
+    //TODO: Refactor to list, for better insertion and removal
+    GameObject[] enemies;
+    GameObject player;
+    bool chasing; 
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("B4"); 
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "B4")
+            TriggerEnemyEvent();
+            
+
+    }
+
+    void FixedUpdate()
+    {
+        if (chasing)
+            UpdateChasePosition(); 
+
+    }
+
+    void UpdateChasePosition()
+    {
+        for(int i = 0; i<enemies.Length; i++)
+        {
+            //remove from enemies[]
+            if (enemies[i] == null || !enemies[i].GetComponent<NavMeshAgent>().enabled)
+                return;
+               
+            Debug.Log("Editing gameObject navmesh: " + enemies[i].name);
+            if (enemies[i].GetComponent<NavMeshAgent>() && player)
+            {
+                enemies[i].GetComponent<EnemyNavmeshScript>().MoveToTarget(player.transform.position);
+            }
+        }
+    }
+
+    void TriggerEnemyEvent()
+    {
+        Debug.Log("Event is firing");
+
+        // Play sound 
+
+        // Adjust light
+
+        // Play animations from enemies
+        chasing = true; 
+    }
+}
